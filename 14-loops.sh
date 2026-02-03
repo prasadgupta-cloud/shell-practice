8b1645fe87a20590749f914b30c4add3920b07ea
T S M A V P GUPTA <prasad.gupta.tammana@kyndryl.com>
2026-02-03 23:12:21 +0530

    actions

--- 14-loops.sh
+++ 14-loops.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
-LOG_FILE="/var/log/shell-script/$0.log"
+LOG_FILE="/var/log/shell-script/14-loops.log"
 
 if [ $USERID -ne 0 ];then
     echo "please run this script with root user access"
@@ -12,16 +12,16 @@ if [ $USERID -ne 0 ];then
 mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
-    if [ $1 -ne 0 ]; then
-        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+    if [ "$1" -ne 0 ]; then
+        echo "$2 ... FAILURE" | tee -a "$LOGS_FILE"
         exit 1
     else
-        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
+        echo "$2 ... SUCCESS" | tee -a "$LOGS_FILE"
     fi
 }
  
-for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+for package in "$@" # sudo sh 14-loops.sh nginx mysql nodejs
 do
-    dnf install $package -y &>>"$LOGS_FILE"
+    dnf install "$package" -y &>>"$LOGS_FILE"
     VALIDATE $? "$package installation"
 done    
